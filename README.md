ab0afa7cd5cba800421f3f8ebed39ad9d5fc4ccc
Michael-Soyka <hihihi.good.67@gmail.com>
2024-07-09 23:02:21 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# source-engine-sponza
-Port of the Crytek Sponza
+# Source Engine Sponza
+Port of the Crytek Sponza to Source Engine 2013
+
+_sources - contains textures + PBR ( combined to MRAO ), if you have the [TheXa4's](https://github.com/thexa4/source-pbr) PBR or own.
+maps - contains .vmf, .bsp.
+materials - contains base materials ( without PBR ) for the map.
+models - contains models for Sponza.
---
+++ _sources/models/generate_qc4all_files.py
@@ -0,0 +1,63 @@
+import os
+import subprocess
+
+COMPILE = True
+
+STUDIO_PATH = r"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Singleplayer\bin\studiomdl.exe"
+GAME_PATH = r"C:\Users\source-engine-slut\Desktop\source-sdk-vs2022\sp\game\mod_hl2"
+FLAGS = "-nop4 -verbose"
+
+basic_path = "props_sponza"
+basic_material_path = None
+
+basic_model_scale = 64
+
+basic_qc_patt = '''\
+$scale {scale}
+$modelname	"{path}\{model}.mdl"
+$body mybody	"{model}"
+
+$staticprop
+
+$cdmaterials	"models\{materials_path}"
+
+$sequence idle	"{model}"\
+'''
+
+for file in os.listdir():
+    if file.endswith( '.smd' ):
+        print( file )
+
+        model_name = file.split( '.' )[0]
+        model_name_qc = model_name + '.qc'
+
+        if not os.path.exists( './' + model_name_qc ):
+            with open( './' + model_name_qc, 'w' ) as out:
+                try:
+                    out.write( basic_qc_patt.format(
+                        scale = basic_model_scale,
+                        path = basic_path,
+                        materials_path = basic_path if not basic_material_path else basic_material_path,
+                        model = model_name
+                        )
+                    )
+
+                    print ( 'Created new QC file ' + model_name )
+                except KeyError as error:
+                    print( "Пизда корытом " + model_name )
+        else:
+            print ( 'QC file ' + model_name + ' exists. Compiling...' )
+
+        if ( COMPILE ):
+
+            process = subprocess.run(
+                [ STUDIO_PATH,
+                 '-game',
+                 FLAGS,
+                 model_name_qc ], capture_output=True)
+
+            if ( process.returncode == 0 ):
+                print( 'Done!' )
+            else:
+                print( 'Smth wrong?' )
+
